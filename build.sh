7b27af46e88143a7f6651a92b99d6f6aedae2452
ax77 <mailbox.r8@gmail.com>
2023-01-27 01:55:24 -0500

    Rename /eureka-discovery-server/ to /service-discovery/.

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 # Copyright (c) 2023. All rights reserved. https://github.com/ax77
 #
 
+#export JAVA_HOME=/usr/lib64/jvm/jre-17-openjdk
+#export PATH=$JAVA_HOME/bin:$PATH
+#source /etc/profile
+
 cd ./applicant-service
 mvn clean install -DskipTests
 cd ..
@@ -14,7 +18,7 @@ cd ./api-gateway
 mvn clean install -DskipTests
 cd ..
 
-cd ./eureka-discovery-server
+cd ./service-discovery
 mvn clean install -DskipTests
 cd ..
 
